4f3980aa7cc6e8b74dc75fbf9c4b10ae0e807b44
marianomontiel <67610069+marianomontiel@users.noreply.github.com>
2023-11-07 23:06:06 -0300

    Fix If Arguments that were inverted

--- script.js
+++ script.js
@@ -27,6 +27,52 @@ function addBill() {
     document.getElementById('billsList').insertAdjacentHTML('beforeend', billHTML);
 }
 
+//fuction with default values
+/* function addBill() {
+    let billHTML = `
+        <div class="bill" id="1">
+            <label for="billName">Bill Name:</label>
+            <input type="text" class="billName" placeholder="Enter bill name" value="pizza">
+            <label for="billAmount">Bill Amount:</label>
+            <input type="number" class="billAmount" placeholder="Enter amount" value="100">
+            <label for="billParticipants">Participants:</label>
+            <input type="text" class="billParticipants" placeholder="Enter participant names" value="a,b,c,d">
+            <label for="payer">Payer:</label>
+            <input type="text" class="payer" placeholder="Enter who paid the bill" value="c">
+            <button onclick="removeBill(1)">x</button>
+        </div>
+        <div class="bill" id="2">
+            <label for="billName">Bill Name:</label>
+            <input type="text" class="billName" placeholder="Enter bill name" value="pizza">
+            <label for="billAmount">Bill Amount:</label>
+            <input type="number" class="billAmount" placeholder="Enter amount" value="100">
+            <label for="billParticipants">Participants:</label>
+            <input type="text" class="billParticipants" placeholder="Enter participant names" value="c,d,e,f">
+            <label for="payer">Payer:</label>
+            <input type="text" class="payer" placeholder="Enter who paid the bill" value="d">
+            <button onclick="removeBill(2)">x</button>
+        </div>
+        <div class="bill" id="3">
+            <label for="billName">Bill Name:</label>
+            <input type="text" class="billName" placeholder="Enter bill name" value="pizza">
+            <label for="billAmount">Bill Amount:</label>
+            <input type="number" class="billAmount" placeholder="Enter amount" value="100">
+            <label for="billParticipants">Participants:</label>
+            <input type="text" class="billParticipants" placeholder="Enter participant names" value="c,d">
+            <label for="payer">Payer:</label>
+            <input type="text" class="payer" placeholder="Enter who paid the bill" value="c">
+            <button onclick="removeBill(3)">x</button>
+        </div>
+    `;
+    document.getElementById('billsList').insertAdjacentHTML('beforeend', billHTML);
+} */
+
+function removeBill(a) {
+    const billDiv = document.getElementById(a);
+    billDiv.remove();
+
+
+}
 
 function settleUp() {
     participants = document.getElementById('participants').value.split(',').map(name => name.trim());
@@ -40,26 +86,28 @@ function settleUp() {
 
     const balances = {};
 
-    //Calculate who owes how much, this is the buggy script.
+    //Calculate who owes how much. This is the buggy script.
     bills.forEach(bill => {
         const perPersonAmount = bill.amount / bill.participants.length;
+        console.log(perPersonAmount);
         bill.participants.forEach(person => {
             if (!balances[person]) {
                 balances[person] = 0;
             }
             if (!bill.participants.includes(bill.payer)) {
+                balances[person] += perPersonAmount;
+                balances[bill.payer] -= bill.amount;
+                console.log("doesnt include bill.payer");
+
+            } else {
                 if (person !== bill.payer) {
                     balances[person] += perPersonAmount;
                 } else if (person === bill.payer) {
                     balances[person] -= bill.amount - perPersonAmount;
                 }
-            } else {
-                console.log("el filtro funca")
-                balances[person] += perPersonAmount;
-                balances[bill.payer] -= bill.amount;
-                
+                console.log("includes bill.payer");
             }
-
+            console.table(balances)
         });
     });
 
